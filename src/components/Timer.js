68c46c327bb3831fb06ec5523b3477a32fa3531a
Fábio Santos <fabiosantosart@gmail.com>
2016-10-07 21:40:14 +0100

    lolada 2

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,18 +4,19 @@ import { connect } from 'react-redux'
 const Timer = React.createClass({
   componentWillMount() {
     this.timer = setInterval(() => {
-      
+      this.setState({ forceUpdateLol: Math.random() })
     }, 1000)
   },
-  componentWillUpdate(nextProps) {
+  componentWillUpdate(nextProps, nextState) {
     const started = Boolean(this.state && this.state.start)
-    if (nextProps && nextProps.currentAlbum !== this.props && this.props.currentAlbum || !started) {
+    if (nextProps && nextProps.currentAlbum !== this.props && this.props.currentAlbum || !started || ((this.state && this.state.forceUpdateLol) !== nextState.forceUpdateLol)) {
       this.setState({ start: Date.now() })
     }
   },
   render() {
     const timeDiff = ((Date.now() - (this.state && this.state.start)) / 1000) /* NaN-killer */ | 0
     const number = ('00' + timeDiff).substr(-2)
+    console.log('start',this.state.start, 'timediff', timeDiff, 'number', number)
     return (
       <div>
         {number}
